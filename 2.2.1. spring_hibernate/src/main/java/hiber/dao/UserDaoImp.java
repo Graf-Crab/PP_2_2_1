4c3873c41b2678e2f23aaa34bdead1ffa0c2d695
Graf-Crab <graf.crab@gmail.com>
2023-07-21 09:17:09 +0300

    next stage

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,13 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public void add(User user) {
+
       sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public void add(Car car) {
+      sessionFactory.getCurrentSession().save(car);
    }
 
    @Override
@@ -24,6 +31,17 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   public List<Car> listCars() {
+      TypedQuery<Car> query=sessionFactory.getCurrentSession().createQuery("from Car");
+      return query.getResultList();
+   }
+
+   @Override
+   public User carList(String model, int series) {
+      return null;
    }
 
 }
